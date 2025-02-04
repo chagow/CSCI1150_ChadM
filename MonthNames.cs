da7618a28e99f3e517b79d6a4b262450ff8631f7
chagow <tshadmoua@gmail.com>
2025-02-04 09:12:46 -0600

    Update MonthNames.cs

--- MonthNames.cs
+++ MonthNames.cs
@@ -3,8 +3,15 @@ using static System.Console;
 using System.Globalization;
 class MonthNames
 {
+	enum Month
+	{
+		JANUARY = 1, FEBRUARY = 2, MARCH = 3, APRIL = 4, MAY = 5, JUNE = 6, JULY = 7, AUGUST = 8, SEPTEMBER = 9, OCTOBER = 10, NOVEMBER = 11, DECEMBER = 12
+	}
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter month number: ");
+		int usernum = Convert.ToInt32(ReadLine());
+		Month month = (Month)Convert.ToInt32(usernum);
+		WriteLine(month);
 	}
 }
