fa79b870c1bef2535a50432884da3f3e3abf9389
chagow <tshadmoua@gmail.com>
2025-02-04 08:49:55 -0600

    Update FahrenheitToCelsius.cs

--- FahrenheitToCelsius.cs
+++ FahrenheitToCelsius.cs
@@ -5,6 +5,12 @@ class FahrenheitToCelsius
 {
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter a temperature in fahrenheit: ");
+		double usertemp = Convert.ToDouble(ReadLine());
+		double tocelsius = 5.0 / 9.0;
+		double subcels = usertemp - 32;
+		double convcels = subcels * tocelsius;
+		Write($"{Math.Round(usertemp, 1):0.0} F is "); 
+		WriteLine($"{Math.Round(convcels, 1):0.0} C");
 	}
 }
