799043dd3f81912831c36e7029c1e0794031cdc2
chagow <tshadmoua@gmail.com>
2025-02-04 10:06:57 -0600

    Update DebugTwo1.cs

--- DebugTwo1.cs
+++ DebugTwo1.cs
@@ -1,17 +1,18 @@
 // Program compares your name to the name of your boss
 using System;
 using static System.Console;
+using System.Globalization;
 class DebugTwo1
 {
    static void Main()
    {
-      string name, bossName,
+      string name, bossName;
       bool areNamesTheSame;
-      Write(Enter your name >> ");
+      Write("Enter your name >> ");
       name = ReadLine();
       Write("Hello {0}! Enter the name of your boss >> ", name);
-      bossName == ReadLine();
+      bossName = ReadLine();
       areNamesTheSame = String.Equals(name, bossName);
-      WriteLine("It is {0} that you are your own boss", isNameTheSame);
+      WriteLine("It is {0} that you are your own boss", areNamesTheSame);
    }
 }
