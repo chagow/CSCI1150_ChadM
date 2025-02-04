804354dd249fe16c3193a28343af9397ccd8a4cb
chagow <tshadmoua@gmail.com>
2025-02-04 05:37:25 -0600

    Update HoursAndMinutes.cs

--- HoursAndMinutes.cs
+++ HoursAndMinutes.cs
@@ -5,6 +5,10 @@ class HoursAndMinutes
 {
 	static void Main()
 	{
-		// Write your code here
+		const double minhour = 60;
+		double minutes = 197;
+		double hour = Math.Floor(minutes / minhour);
+		double realminutes = minutes - minhour * hour;
+		WriteLine(minutes+ " minutes is " +hour+ " hours and " +realminutes+ " minutes");
 	}
 }
