c3fc738104ab4afeb263689ddebda17597e67af9
chagow <tshadmoua@gmail.com>
2025-02-04 16:15:01 -0600

    Update MarshallsRevenue.cs

--- MarshallsRevenue.cs
+++ MarshallsRevenue.cs
@@ -5,6 +5,23 @@ class MarshallsRevenue
 {
    static void Main()
    {
-      // Write your code here
+      double intmural, extmural, intrevenue, extrevenue, totalrev;
+      Write("Enter number of interior murals scheduled: ");
+      intmural = Convert.ToDouble(ReadLine());
+      Write("Enter number of exterior murals scheduled: ");
+      extmural = Convert.ToDouble(ReadLine());
+      const double intcost = 500, extcost = 750;
+      intrevenue = intmural * intcost;
+      extrevenue = extmural * extcost;
+      totalrev = intrevenue + extrevenue;
+      bool fact = intmural > extmural;
+      WriteLine("{0} interior murals are scheduled at {1} each for a total of {2}", intmural, 
+                intcost.ToString("C", CultureInfo.GetCultureInfo("en-US")),
+                intrevenue.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+      WriteLine("{0} exterior murals are scheduled at {1} each for a total of {2}", extmural, 
+                extcost.ToString("C", CultureInfo.GetCultureInfo("en-US")),
+                extrevenue.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+      WriteLine("Total revenue expected is {0}", totalrev.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+      WriteLine("It is " +fact+ " that there are more interior murals scheduled than exterior ones.");
    }
 }
