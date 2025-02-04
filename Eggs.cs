7015b7ffb0cf7f8387631358d97773e4358782c8
chagow <tshadmoua@gmail.com>
2025-02-04 05:54:27 -0600

    Update Eggs.cs

--- Eggs.cs
+++ Eggs.cs
@@ -5,6 +5,11 @@ class Eggs
 {
 	static void Main()
 	{
-		// Write your code here
+		int chicken1 = 32, chicken2 = 28, chicken3 = 17, chicken4 = 50;
+		const int todozen = 12;
+		int totalegg = chicken1 + chicken2 + chicken3 + chicken4;
+		int dozen = totalegg / todozen;
+		int realegg = totalegg - todozen * dozen;
+		WriteLine(totalegg+ " eggs is " +dozen+ " dozen and " +realegg+ " eggs");
 	}
 }
