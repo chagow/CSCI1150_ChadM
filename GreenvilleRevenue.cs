091439644749d22da97cc08ccc8d962403b98d7d
chagow <tshadmoua@gmail.com>
2025-02-04 15:48:19 -0600

    Update GreenvilleRevenue.cs

--- GreenvilleRevenue.cs
+++ GreenvilleRevenue.cs
@@ -5,6 +5,16 @@ class GreenvilleRevenue
 {
    static void Main()
    {
-      // Write your code here
+      double oldcon, newcon, revenue;
+      Write("Enter number of contestants last year: ");
+      oldcon = Convert.ToDouble(ReadLine());
+      Write("Enter number of contestants this year: ");
+      newcon = Convert.ToDouble(ReadLine());
+      const double fee = 25;
+      revenue = newcon * fee;
+      bool fact = newcon > oldcon;
+      WriteLine("Last year's competition had {0} contestants, and this year's has {1} contestants", oldcon, newcon);
+      WriteLine("Revenue expected this year is {0}", revenue.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+      WriteLine("It is " +fact+ " that this year's competition is bigger than last year's");
    }
 }
