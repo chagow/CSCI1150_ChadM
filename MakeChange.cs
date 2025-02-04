b55891cc1671d544c70c05263c23f2498c2c5da6
chagow <tshadmoua@gmail.com>
2025-02-04 06:24:05 -0600

    Update MakeChange.cs

--- MakeChange.cs
+++ MakeChange.cs
@@ -5,6 +5,13 @@ class MakeChange
 {
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter an amount of dollars: ");
+		int userdoll = Convert.ToInt32(ReadLine());
+		int twenty = Convert.ToInt32(userdoll / 20);
+		int ten = Convert.ToInt32((userdoll - 20 * twenty) / 10);
+		int five = Convert.ToInt32((userdoll - 20 * twenty - 10 * ten) / 5);
+		int one = Convert.ToInt32((userdoll - 20 * twenty - 10 * ten - 5 * five) / 1);
+
+		WriteLine("twenties: " +twenty+ " tens: " +ten+ " fives: " +five+ " ones: " +one);
 	}
 }
