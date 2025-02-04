5b824ccaf2c84ab009990a0bc0a6bbb2110366ed
chagow <tshadmoua@gmail.com>
2025-02-04 15:21:58 -0600

    Update DebugTwo3.cs

--- DebugTwo3.cs
+++ DebugTwo3.cs
@@ -8,23 +8,22 @@ class DebugTwo3
 {
    static void Main()
    {
-      const double WITHHOLDING_RATE = 15;
+      const double WITHHOLDING_RATE = 0.15;
       string hoursAsString, rateAsString;
-      double hours, rate;
-      double gross, withholding, net;
+      double hours, rate, gross, withholding, net;
       Write("Enter the number of hours you worked this week >> ");
-      hoursasString = ReadLine();
+      hoursAsString = ReadLine();
       Write("Enter your hourly rate >> ");
-      rateAsString = ReadLine()
-      hours = Convert.ToDouble(hoursAsStrinng);
-      rate = Convert.ToDouble(rateAsStrinng);
-      gross = hours + rate;
+      rateAsString = ReadLine();
+      hours = Convert.ToDouble(hoursAsString);
+      rate = Convert.ToDouble(rateAsString);
+      gross = hours * rate;
       withholding = gross * WITHHOLDING_RATE;
-      net = gross * withholding;
+      net = gross - withholding;
       WriteLine("You worked {0} hours at {1} per hour",
 	 hours, rate.ToString("C", CultureInfo.GetCultureInfo("en-US")));
       WriteLine("Gross pay is {0}", gross.ToString("C", CultureInfo.GetCultureInfo("en-US")));
       WriteLine("Withholding is {0}", withholding.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-      WriteLine("Net pay is {0}", netToString("C", CultureInfo.GetCultureInfo("en-US"))); 
+      WriteLine("Net pay is {0}", net.ToString("C", CultureInfo.GetCultureInfo("en-US"))); 
    }
 }
