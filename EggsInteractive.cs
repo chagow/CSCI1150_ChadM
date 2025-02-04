f18979f18a32ff9d3e6d38a13de51d564a0dbacb
chagow <tshadmoua@gmail.com>
2025-02-04 06:01:32 -0600

    Update EggsInteractive.cs

--- EggsInteractive.cs
+++ EggsInteractive.cs
@@ -5,6 +5,19 @@ class EggsInteractive
 {
 	static void Main()
 	{
-		// Write your code here
+		int chicken1, chicken2, chicken3, chicken4 ;
+		Write("How many eggs did chicken 1 lay? ");
+		chicken1 = Convert.ToInt32(ReadLine());
+		Write("How many eggs did chicken 2 lay? ");
+		chicken2 = Convert.ToInt32(ReadLine());
+		Write("How many eggs did chicken 3 lay? ");
+		chicken3 = Convert.ToInt32(ReadLine());
+		Write("How many eggs did chicken 4 lay? ");
+		chicken4 = Convert.ToInt32(ReadLine());
+		const int todozen = 12;
+		int totalegg = chicken1 + chicken2 + chicken3 + chicken4;
+		int dozen = totalegg / todozen;
+		int realegg = totalegg - todozen * dozen;
+		WriteLine(totalegg+ " eggs is " +dozen+ " dozen and " +realegg+ " eggs");
 	}
 }
