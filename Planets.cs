4cc408a9d50a5cac6933452f8a6b60a14cd5d26b
chagow <tshadmoua@gmail.com>
2025-02-04 09:24:54 -0600

    Update Planets.cs

--- Planets.cs
+++ Planets.cs
@@ -3,8 +3,15 @@ using static System.Console;
 using System.Globalization;
 class Planets
 {
+	enum Planet
+	{
+		Mercury = 1, Venus = 2, Earth = 3, Mars = 4, Jupiter = 5, Saturn = 6, Uranus = 7, Neptune = 8
+	}
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter planet number: ");
+		int usernum = Convert.ToInt32(ReadLine());
+		Planet planet = (Planet)Convert.ToInt32(usernum);
+		WriteLine(planet+ " is " +usernum+ " planet(s) from the sun");
 	}
 }
