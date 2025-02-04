2f7a484813d746a9a2b04dd4dcf8af158b00b758
chagow <tshadmoua@gmail.com>
2025-02-04 04:22:26 -0600

    Update ProjectedRaisesInteractive.cs

--- ProjectedRaisesInteractive.cs
+++ ProjectedRaisesInteractive.cs
@@ -5,16 +5,21 @@ class ProjectedRaisesInteractive
 {
 	static void Main()
 	{
-		const double raise = 0.04;
 		Write("Enter the salary for employee 1: ");
-		double emp1 = Convert.ToDouble(ReadLine());
+		double empone = Convert.ToDouble(ReadLine());
 		Write("Enter the salary for employee 2: ");
-		double emp2 = Convert.ToDouble(ReadLine());
+		double emptwo = Convert.ToDouble(ReadLine());
 		Write("Enter the salary for employee 3: ");
-		double emp1 = Convert.ToDouble(ReadLine());
+		double empthr = Convert.ToDouble(ReadLine());
+		const double raise = 0.04;
+
+		double emp1 = empone + empone * raise;
+		double emp2 = emptwo + emptwo * raise;
+		double emp3 = empthr + empthr * raise;
 
 		WriteLine("Next year's salary for the first employee will be {0}", emp1.ToString("C", CultureInfo.GetCultureInfo("en-US")));
 		WriteLine("Next year's salary for the second employee will be {0}", emp2.ToString("C", CultureInfo.GetCultureInfo("en-US")));
 		WriteLine("Next year's salary for the third employee will be {0}", emp3.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+
 	}
 }
