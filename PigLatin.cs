6e4f442ef4376932129c215b75564370b6e2ae71
chagow <tshadmoua@gmail.com>
2025-02-04 09:54:36 -0600

    Update PigLatin.cs

--- PigLatin.cs
+++ PigLatin.cs
@@ -5,6 +5,10 @@ class PigLatin
 {
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter a word: ");
+		string userword = ReadLine();
+		string firstletter = userword.Substring(0, 1);
+		string leftover = userword.Substring(1, userword.Length - 1);
+		WriteLine(leftover+firstletter+"ay");
 	}
 }
