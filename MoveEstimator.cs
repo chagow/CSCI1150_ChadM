bc37798881ac4d59dc08b975190516988facbcad
chagow <tshadmoua@gmail.com>
2025-02-04 04:38:40 -0600

    Update MoveEstimator.cs

--- MoveEstimator.cs
+++ MoveEstimator.cs
@@ -5,6 +5,24 @@ class MoveEstimator
 {
 	static void Main()
 	{
-		// Write your code here
+		const double move = 200;
+		const double perhour = 150;
+		const double permile = 2;
+
+		Write("Enter number of hours: ");
+		double userhour = Convert.ToDouble(ReadLine());
+		Write("Enter number of miles: ");
+		double usermile = Convert.ToDouble(ReadLine());
+
+		double hourcost = userhour * perhour;
+		double milecost = usermile * permile;
+		double total = move + hourcost + milecost;
+
+		Write("For a move taking ");
+		Write(userhour);
+		Write(" hours and going ");
+		Write(usermile);
+		Write(" miles the estimate is ");
+		WriteLine("{0}", total.ToString("C", CultureInfo.GetCultureInfo("en-US")));
 	}
 }
