0d3fed0618637347d05c5aa8239bd99347dca162
chagow <tshadmoua@gmail.com>
2025-02-04 03:31:44 -0600

    Update ProjectedRaises.cs

--- ProjectedRaises.cs
+++ ProjectedRaises.cs
@@ -5,6 +5,13 @@ class ProjectedRaises
 {
 	static void Main()
 	{
-		// Write your code here
+		const double raise = 0.04;
+		double emp1 = 25000 + 25000 * raise;
+		double emp2 = 38000 + 38000 * raise;
+		double emp3 = 51000 + 51000 * raise;
+
+		WriteLine("Next year's salary for the first employee will be {0}", emp1.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+		WriteLine("Next year's salary for the second employee will be {0}", emp2.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+		WriteLine("Next year's salary for the third employee will be {0}", emp3.ToString("C", CultureInfo.GetCultureInfo("en-US")));
 	}
 }
