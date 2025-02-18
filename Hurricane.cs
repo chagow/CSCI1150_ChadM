f562f4116e41ffd36f02e24473ea6d595222c5fd
chagow <tshadmoua@gmail.com>
2025-02-17 22:35:17 -0600

    Update Hurricane.cs

--- Hurricane.cs
+++ Hurricane.cs
@@ -5,6 +5,27 @@ class Hurricane
 {
 	static void Main()
 	{
-		// Write your code here
+		int user_wind;
+		Write("What is the windspeed of the Hurricane?: ");
+		user_wind = Convert.ToInt32(ReadLine());
+		if (user_wind < 74){
+			WriteLine("This is not a hurricane ");
+		} else{
+			if(user_wind < 96 && user_wind >= 74){
+				WriteLine("This is a category 1 hurricane ");
+			}
+			if(user_wind < 111 && user_wind >= 96){
+				WriteLine("This is a category 2 hurricane ");
+			}
+			if(user_wind < 130 && user_wind >= 111){
+				WriteLine("This is a category 3 hurricane ");
+			}
+			if(user_wind < 157 && user_wind >= 130){
+				WriteLine("This is a category 4 hurricane ");
+			}
+			if(user_wind >= 157){
+				WriteLine("This is a category 5 hurricane ");
+			}
+		}
 	}
 }
