0f25b8e6aeef57a0f664a844d5ab90cbe9d6317a
chagow <tshadmoua@gmail.com>
2025-02-18 02:38:06 -0600

    Update CheckMonth2.cs

--- CheckMonth2.cs
+++ CheckMonth2.cs
@@ -5,6 +5,99 @@ class CheckMonth2
 {
 	static void Main()
 	{
-		// Write your code here
+		int user_month, user_day;
+		Write("Enter your birth month number: ");
+		user_month = Convert.ToInt32(ReadLine());
+		Write("Enter your birth day number: ");
+		user_day = Convert.ToInt32(ReadLine());
+		switch (user_month){
+			case 1:
+			if (user_day <= 31 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 2:
+			if (user_day <= 29 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 3:
+			if (user_day <= 31 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 4:
+			if (user_day <= 30 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 5:
+			if (user_day <= 31 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 6:
+			if (user_day <= 30 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 7:
+			if (user_day <= 31 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 8:
+			if (user_day <= 31 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 9:
+			if (user_day <= 30 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 10:
+			if (user_day <= 31 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 11:
+			if (user_day <= 30 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			case 12:
+			if (user_day <= 31 && user_day >= 1){
+				WriteLine(user_month+"/"+user_day+" is a valid birthday");
+				break;
+			} else{
+				WriteLine("Invalid date");
+				break;}
+			default:
+			WriteLine("Invalid date");
+			break;
+		}
 	}
 }
