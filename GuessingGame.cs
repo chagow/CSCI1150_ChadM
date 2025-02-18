bc0a04f48d8ed2df65d47633cb00190d592a017b
chagow <tshadmoua@gmail.com>
2025-02-18 15:25:57 -0600

    Update GuessingGame.cs

--- GuessingGame.cs
+++ GuessingGame.cs
@@ -5,6 +5,20 @@ class GuessingGame
 {
 	static void Main()
 	{
-		// Write your code here
+		Random ranNumberGenerator = new Random();
+		int randomNumber, user_guess;
+		randomNumber = ranNumberGenerator.Next(1, 10);
+		Write("Guess a number from 1 - 10: ");
+		user_guess = Convert.ToInt32(ReadLine());
+		if(user_guess > randomNumber){
+			WriteLine("Too high");
+		}
+		if(user_guess < randomNumber){
+			WriteLine("Too low");
+		}
+		if(user_guess == randomNumber){
+			WriteLine("Correct");
+		}
+		WriteLine(randomNumber);
 	}
 }
