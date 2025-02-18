12df25c87c86258f36f331ecac637c729c75e52a
chagow <tshadmoua@gmail.com>
2025-02-17 21:45:36 -0600

    Update Admission.cs

--- Admission.cs
+++ Admission.cs
@@ -5,6 +5,16 @@ class Admission
 {
 	static void Main()
 	{
-		// Write your code here
+		int user_score;
+		double user_gpa;
+		Write("Enter high school GPA: ");
+		user_gpa = Convert.ToDouble(ReadLine());
+		Write("Enter admission test score: ");
+		user_score = Convert.ToInt32(ReadLine());
+		if (user_gpa >= 3.0 && user_score >= 60 || user_gpa < 3.0 && user_score >= 80){
+			WriteLine("Accept");
+		} else{
+			WriteLine("Reject");
+		}
 	}
 }
