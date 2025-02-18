25c2d52384551c76791fa197b7527b540f5565ad
chagow <tshadmoua@gmail.com>
2025-02-18 15:03:43 -0600

    Update CheckDigit.cs

--- CheckDigit.cs
+++ CheckDigit.cs
@@ -5,6 +5,25 @@ class CheckDigit
 {
 	static void Main()
 	{
-		// Write your code here
+		int user_acc, dividend, remainder;
+		Write("Enter your four-dight account number: ");
+		user_acc = Convert.ToInt32(ReadLine());
+		string acc_dividend = Convert.ToString(user_acc).Substring(0, 3);
+		dividend = Convert.ToInt32(acc_dividend);
+		remainder = dividend % 7;
+		string acc_remainder = Convert.ToString(remainder);
+		string acc_size = Convert.ToString(user_acc);
+		WriteLine(remainder);
+		WriteLine(acc_remainder);
+		WriteLine(acc_size);
+		if (acc_size.Length > 4 || acc_size.Length < 4){
+			WriteLine("Account number invalid - it must have 4 digits.");
+		} else{
+			if(acc_size.EndsWith(acc_remainder)){
+				WriteLine("The account number is valid");
+			} else{
+				WriteLine("Invalid");
+			}
+		}
 	}
 }
