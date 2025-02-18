5ca56a75094ccd56d4d1be6f617d50ad9864e552
chagow <tshadmoua@gmail.com>
2025-02-17 23:08:38 -0600

    Update CheckMonth.cs

--- CheckMonth.cs
+++ CheckMonth.cs
@@ -5,6 +5,13 @@ class CheckMonth
 {
 	static void Main()
 	{
-		// Write your code here
+		int user_month;
+		WriteLine("Enter the number of your birth month: ");
+		user_month = Convert.ToInt32(ReadLine());
+		if(user_month > 12 || user_month < 1){
+			WriteLine("Invalid month");
+		} else{
+			WriteLine(user_month+" is a valid month");
+		}
 	}
 }
