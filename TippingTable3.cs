ec6efcf904e4061b345651f6dea3acaa8e7b4966
chagow <tshadmoua@gmail.com>
2025-02-26 07:56:41 -0600

    Update TippingTable3.cs

--- TippingTable3.cs
+++ TippingTable3.cs
@@ -5,6 +5,21 @@ class TippingTable3
 {
 	static void Main()
 	{
-		// Write your code here
+		double [] billings = new double[2];
+		double [] tippercent = new double[2];
+		for(int i = 0; i < 2; i++){
+			Write("Insert restaurant bill {0} : ", i+1);
+			billings[i] = Convert.ToDouble(ReadLine());
+		}
+		for(int i = 0; i < 2; i++){
+			Write("Insert tipping percentage {0} : ", i+1);
+			tippercent[i] = Convert.ToDouble(ReadLine());
+		}
+		foreach(double bill in billings){
+			foreach(double percent in tippercent){
+				double tipamount = bill * percent;
+				Write("{0} ", tipamount.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+			}
+		}
 	}
 }
