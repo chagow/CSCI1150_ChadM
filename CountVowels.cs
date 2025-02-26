46207038c86456fc993d9318ad3501cc838b5a51
chagow <tshadmoua@gmail.com>
2025-02-26 08:27:06 -0600

    Update CountVowels.cs

--- CountVowels.cs
+++ CountVowels.cs
@@ -5,6 +5,16 @@ class CountVowels
 {
 	static void Main()
 	{
-		// Write your code here
+		string input, lowered;
+		int total = 0;
+		Write("Insert a phrase: ");
+		input = ReadLine();
+		lowered = input.ToLower();
+		for(int i = 0; i < lowered.Length; i++){
+			if(lowered[i] == 'a' || lowered[i] == 'e' || lowered[i] == 'i' || lowered[i] == 'o' || lowered[i] == 'u'){
+				total++;
+			}
+		}
+		WriteLine("Total amount of vowels: {0}", total);
 	}
 }
