1a7ecf560c79617c138246da050e704059276bc8
chagow <tshadmoua@gmail.com>
2025-02-26 15:02:34 -0600

    Update GuessingGame2.cs

--- GuessingGame2.cs
+++ GuessingGame2.cs
@@ -5,6 +5,19 @@ class GuessingGame2
 {
 	static void Main()
 	{
-		// Write your code here
+		Random ranNumberGenerator = new Random();
+		int randomNumber, user_guess;
+		int loop = 0;
+		randomNumber = ranNumberGenerator.Next(1, 10);
+		Write("Guess a number from 1 - 10: ");
+		while(loop == 0){
+			user_guess = Convert.ToInt32(ReadLine());
+			if(user_guess == randomNumber){
+				WriteLine("Correct");
+				loop += 1;
+			} else{
+				Write("Incorrect, guess again: ");
+			}
+		}
 	}
 }
