4e22680148aac8d76ec5fb1f46abc1b448cb61d0
chagow <tshadmoua@gmail.com>
2025-02-25 22:04:54 -0600

    Update EnterUppercaseLetters.cs

--- EnterUppercaseLetters.cs
+++ EnterUppercaseLetters.cs
@@ -5,6 +5,17 @@ class EnterUppercaseLetters
 {
 	static void Main()
 	{
-		// Write your code here
+		string begin_user, uppercase_user;
+		int loop = 0;
+		while (loop == 0){
+			WriteLine("Enter an uppercase letter or ! to quit");
+			uppercase_user = Convert.ToString(ReadLine());
+			if(uppercase_user.Length == 1 && (char.IsUpper(uppercase_user, 0) || uppercase_user.Equals("!"))){
+				WriteLine("OK");
+				loop += 1;
+			} else{
+				WriteLine("Sorry - that was not an uppercase letter. ");
+			}	
+		}
 	}
 }
