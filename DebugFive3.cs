ae050a2204a123443f0a83af447607b480465f1f
chagow <tshadmoua@gmail.com>
2025-02-26 17:03:37 -0600

    Update DebugFive3.cs

--- DebugFive3.cs
+++ DebugFive3.cs
@@ -2,22 +2,25 @@
 // ID number is a letter, followed by a two-digit number
 // For example -- A00 or Z99
 // Go to a new display line after every 20 IDs
+using System;
 using static System.Console;
+using System.Globalization;
 class DebugFive3
 {
    static void Main()
    {
       char letter;
-      int number;
-      const int LOW == 0;
-      const int HIGH == 99;
+      int number = 0;
+      const int LOW = 0;
+      const int HIGH = 99;
       const int NUMINROW = 20;
-      for(letter = 'A'; letter < 'Z'; ++letter)
-        for(number = LOW; number > HIGH; ++number)
-        {
-           if(number % NUMINROW0)
-              WriteLine();
-           Write("{0}{{1} ", letter, number.ToString("D2"));
-        }
+      for(letter = 'A'; letter <= 'Z'; letter++){
+         for(number = LOW; number <= HIGH; number++){
+            if((number % NUMINROW) == 0){
+               WriteLine("");
+            }
+            Write("{0}{1} ", letter, number.ToString("D2"));
+         }
+      }
    }
 }
