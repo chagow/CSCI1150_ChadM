eb28669c6bb73a15e73c7480987f8898392d6652
chagow <tshadmoua@gmail.com>
2025-02-25 21:07:59 -0600

    Update SumInts.cs

--- SumInts.cs
+++ SumInts.cs
@@ -5,6 +5,19 @@ class SumInts
 {
 	static void Main()
 	{
-		// Write your code here
+		int sum = 0;
+		int user_number;
+		int loops = 0;
+		while (loops == 0){
+			WriteLine("Enter a number,\nEntering 999 quits the program: ");
+			user_number = Convert.ToInt32(ReadLine());
+			if(user_number == 999){
+				WriteLine("Total sum, not including 999: "+sum);
+				loops += 1;
+			} else {
+				sum += user_number;
+				WriteLine("Current sum: "+sum);
+			}
+		}
 	}
 }
