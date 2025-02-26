fa01465f5f6e85033f5532e1c19f662d98309754
chagow <tshadmoua@gmail.com>
2025-02-25 20:46:20 -0600

    Update SumFiveInts.cs

--- SumFiveInts.cs
+++ SumFiveInts.cs
@@ -5,6 +5,14 @@ class SumFiveInts
 {
 	static void Main()
 	{
-		// Write your code here
+		int sum = 0;
+		int total = 0;
+		WriteLine("Enter 5 numbers: ");
+		do
+		{
+			sum = sum + (Convert.ToInt32(ReadLine()));
+			total += 1;
+		} while(total < 5);
+		WriteLine("Total sum: "+sum);
 	}
 }
