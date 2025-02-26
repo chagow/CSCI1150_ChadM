aad045b521eface07085aaab6262de261edcccb4
chagow <tshadmoua@gmail.com>
2025-02-26 05:46:56 -0600

    Update Perfect.cs

--- Perfect.cs
+++ Perfect.cs
@@ -5,6 +5,18 @@ class Perfect
 {
 	static void Main()
 	{
-		// Write your code here
+		for(int i = 1; i <= 10000; i++){
+			int sum = 0;
+			int a = 1;
+			while(a < i){
+				if(i % a == 0){
+					sum += a;
+				}
+				a++;
+			}
+			if(sum == i){
+				Write(i+" ");
+			}
+		}
 	}
 }
