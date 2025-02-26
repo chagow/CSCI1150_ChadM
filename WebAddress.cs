6d5b80430e57a0046ad6396a5ab6498ec6ecbf31
chagow <tshadmoua@gmail.com>
2025-02-26 08:10:08 -0600

    Update WebAddress.cs

--- WebAddress.cs
+++ WebAddress.cs
@@ -5,6 +5,10 @@ class WebAddress
 {
 	static void Main()
 	{
-		// Write your code here
+		string user_web;
+		Write("What website are you looking for: ");
+		user_web = ReadLine();
+		user_web = user_web.Replace(" ", "");
+		WriteLine("www."+user_web+".com");
 	}
 }
