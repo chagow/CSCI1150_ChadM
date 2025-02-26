68195948395437831ee8c85c8872da1b26153188
chagow <tshadmoua@gmail.com>
2025-02-26 15:47:52 -0600

    Update GuessingGame3.cs

--- GuessingGame3.cs
+++ GuessingGame3.cs
@@ -5,6 +5,32 @@ class GuessingGame3
 {
 	static void Main()
 	{
-		// Write your code here
+		Random ranNumberGenerator = new Random();
+		int randomNumber, user_guess;
+		int again = 0;
+		int loop = 0;
+		randomNumber = ranNumberGenerator.Next(1, 10);
+		Write("Guess a number from 1 - 10: ");
+		while(loop == 0){
+			user_guess = Convert.ToInt32(ReadLine());
+			if(user_guess == randomNumber){
+				WriteLine("Correct");
+				loop += 1;
+			}
+			if(user_guess > randomNumber){
+				if(again == user_guess){
+					WriteLine("You already knew it was not "+again+" or more");
+				}
+				WriteLine("Your guess was too high");
+				again = user_guess;
+			}
+			if(user_guess < randomNumber){
+				if(again == user_guess){
+					WriteLine("You already knew it was not "+again+" or less");
+				}
+				WriteLine("You guess was too low");
+				again = user_guess;
+			}
+		}
 	}
 }
