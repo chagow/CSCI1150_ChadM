1ea35922777b04a4a30ebb76fdbe5c5712cb422e
chagow <tshadmoua@gmail.com>
2025-02-25 22:33:18 -0600

    Update DailyTemps.cs

--- DailyTemps.cs
+++ DailyTemps.cs
@@ -5,6 +5,26 @@ class DailyTemps
 {
 	static void Main()
 	{
-		// Write your code here
+		double tempinput, tempavg;
+		double tempsum = 0;
+		double tempamount = 0;
+		double loop = 0;
+		WriteLine("Valid temperatures range from -20 to 130, Enter a temperature number (Enter 999 to quit): ");
+		while (loop == 0){
+			tempinput = Convert.ToDouble(ReadLine());
+			if (tempinput == 999){
+				tempavg = tempsum / tempamount;
+				WriteLine("You have inputted: "+tempamount+" temperatures that averages: "+tempavg);
+				loop += 1;
+			} else{
+				if(tempinput < -20 || tempinput > 130){
+					WriteLine("Valid temperatures range from -20 to 130. Please reenter temperature. ");
+				} else{
+					tempamount++;
+					tempsum += tempinput;
+					WriteLine("Current amount: "+tempamount+"\nCurrent sum: "+tempsum);
+				}
+			} 
+		}
 	}
 }
