a8923d6cf2d0dbcf81e43a409006c72825dcc6a9
chagow <tshadmoua@gmail.com>
2025-02-26 16:13:03 -0600

    Update DebugFive1.cs

--- DebugFive1.cs
+++ DebugFive1.cs
@@ -1,21 +1,24 @@
 // Program asks user to enter password
 // If password is not "home", "lady" or "mouse"
 // the user must re-enter the password
+using System;
 using static System.Console;
+using System.Globalization;
 class DebugFive1
 {
    static void Main()
    {
-      cont string PASS1 = "home";
-      cont string PASS2 = "lady";
-      cont string PASS3 = "mouse";
+      const string PASS1 = "home";
+      const string PASS2 = "lady";
+      const string PASS3 = "mouse";
       string password;
 
       Write("Please enter your password ");
       password = Console.ReadLine();
-      while(password != PASS1 || password != PASS2 || password != PASS3)
+      while(password != PASS1 && password != PASS2 && password != PASS3){
          WriteLine("Invalid password. Please enter again. ");
-      password = ReadLine();
+         password = Console.ReadLine();
+      }
       WriteLine("Valid password");
    }
 }
