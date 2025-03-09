1af3e7d65062d6ac205b744c6c50c462499cc04f
chagow <tshadmoua@gmail.com>
2025-03-09 16:35:51 -0500

    Update TestScoreList.cs

--- TestScoreList.cs
+++ TestScoreList.cs
@@ -5,6 +5,21 @@ class TestScoreList
 {
 	static void Main()
 	{
-		// Write your code here
+		double scoreInput, scoreSum = 0;
+		double scoreAvg;
+		double[] scoreList = new double[8];
+		for(int i = 0; i < scoreList.Length; i++){
+			Write("Enter your score: ");
+			scoreInput = Convert.ToDouble(ReadLine());
+			scoreSum += scoreInput;
+			scoreList[i] = scoreInput;
+		}
+		
+		WriteLine("");
+		scoreAvg = scoreSum / scoreList.Length;
+
+		for(int i = 0; i < scoreList.Length; i++){
+			WriteLine("Test # "+(i)+":	"+scoreList[i]+" From average: "+(scoreList[i] - scoreAvg));
+		}
 	}
 }
