1fe92e50fb34ccb10b6ea59f8cbb85accedd06e6
chagow <tshadmoua@gmail.com>
2025-03-10 01:52:44 -0500

    Update CheckZips.cs

--- CheckZips.cs
+++ CheckZips.cs
@@ -6,7 +6,25 @@ class CheckZips
 	static void Main()
 	{
 		string[] zips = {"12789", "54012", "54481", "54982", "60007",
-         "60103", "60187", "60188", "71244", "90210"};
-		// Write your code here
+	        "60103", "60187", "60188", "71244", "90210"};
+			
+		string userInput;
+	        bool check = false;
+	        WriteLine("Enter a valid zip code: ");
+	        userInput = ReadLine();
+	        for(int i = 0; i < zips.Length; i++){
+	            if(userInput == zips[i]){
+	                check = true;
+	                break;
+	            }
+	            else{
+	                check = false;
+	            }
+	        }
+	        if(check == true){
+	            WriteLine("Delivery to "+userInput+" ok");
+	        } else{
+	            WriteLine("Sorry - no delivery to "+userInput);
+	        }
 	}
 }
