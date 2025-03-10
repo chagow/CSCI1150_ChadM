292c3609ee9cf94fb8a878b5a3f9fd30eac09066
chagow <tshadmoua@gmail.com>
2025-03-10 04:21:35 -0500

    Update ResortPrices.cs

--- ResortPrices.cs
+++ ResortPrices.cs
@@ -5,6 +5,20 @@ class ResortPrices
 {
 	static void Main()
 	{
-		// Write your code here
+	    double[] preNights = {1, 3, 5, 8};
+	    double[] perNightCost = {200, 180, 160, 145};
+	    int nightCheck = preNights.Length - 1;
+	    double userNights = 0;
+	    double totalCost;
+	    Write("How many nights are you staying: ");
+	    userNights = Convert.ToDouble(ReadLine());
+	    while(nightCheck >= 0 && userNights < preNights[nightCheck]){
+	        --nightCheck;
+	    }
+	    totalCost = perNightCost[nightCheck] * userNights;
+	    WriteLine("Price per night is {0}\nTotal for {1} night(s) is {2}",
+            perNightCost[nightCheck].ToString("C", CultureInfo.GetCultureInfo("en-US")),
+            userNights,
+            totalCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
 	}
 }
