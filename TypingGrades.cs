25a40f2a6a77fd3f75913d2967c2c6721a42b96a
chagow <tshadmoua@gmail.com>
2025-03-10 02:57:35 -0500

    Update TypingGrades.cs

--- TypingGrades.cs
+++ TypingGrades.cs
@@ -5,6 +5,17 @@ class TypingGrades
 {
 	static void Main()
 	{
-		// Write your code here
+		double[] wordsTyped = {0, 16, 31, 51, 76};
+	        string[] grade = {"F", "D", "C", "B", "A" };
+	        int gradeCheck = wordsTyped.Length - 1;
+	        double studentTyped = 0;
+	        string finalGrade;
+	        Write("How many words did you type: ");
+	        studentTyped = Convert.ToDouble(ReadLine());
+	        while(gradeCheck >= 0 && studentTyped < wordsTyped[gradeCheck]){
+	            --gradeCheck;
+	        }
+	        finalGrade = grade[gradeCheck];
+	        WriteLine("Typing "+studentTyped+" per minute: Grade: "+finalGrade);
 	}
 }
