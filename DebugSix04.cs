cfa2995f9238324e8c78eaeea0c705202975c288
chagow <tshadmoua@gmail.com>
2025-03-10 16:36:11 -0500

    Update DebugSix04.cs

--- DebugSix04.cs
+++ DebugSix04.cs
@@ -3,36 +3,34 @@
 */ 
 using System;
 using static System.Console;
+using System.Globalization;
 class DebugSix04
 {
    public static void Main()
    {
       const int QUIT = 999;
-      int[] numbers = new int[];
-      int x;
-      int num;
+      int[] numbers = new int[20];
+      int x = 0;
+      int num = 0;
       double average;
       double total = 0;
       string inString;
-      Console.Write("Please enter a number or " +
-         QUIT + " to quit...");
-      inString = ReadLine;
-      num = Convert.ToInt(inString);
-      while((x < numbers.Length) && num == QUIT)
+      
+      while((x < numbers.Length) || num == QUIT)
       {
- 	  numbers[x] = num;
-          total += numbers[x];
-          ++x;
-          Write("Please enter a number or " +
-             QUIT + " to quit...");
-          inString = readLine();
-          num = Convert.ToInt32(inString);
+        Write("Please enter a number or " +
+            QUIT + " to quit... ");
+        inString = ReadLine();
+        num = Convert.ToInt32(inString);
+ 	     numbers[x] = num;
+        total += numbers[x];
+        ++x;
       }
       WriteLine("The numbers are:");
-      for(int y = 0; y < x; ++x)
-          Console.Write("{0,6}", numbers[y]);
-      average = total / z;
+      for(int y = 0; y < (x); ++y)
+          Console.Write("{0} ", numbers[y]);
+      average = total / numbers.Length;
       WriteLine();
-      WriteLine("The average is {0}", avge;
+      WriteLine("The average is {0}", average);
   }
 }
