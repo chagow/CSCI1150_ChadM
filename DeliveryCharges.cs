19671f50e4507b9ff576f402bbb8c1f02a658206
chagow <tshadmoua@gmail.com>
2025-03-10 01:50:41 -0500

    Update DeliveryCharges.cs

--- DeliveryCharges.cs
+++ DeliveryCharges.cs
@@ -5,6 +5,26 @@ class DeliveryCharges
 {
 	static void Main()
 	{
-		// Write your code here
+		string[] zips = {"12789", "54012", "54481", "54982", "60007",
+	        "60103", "60187", "60188", "71244", "90210"};
+			
+		string userInput;
+	        bool check = false;
+	        WriteLine("Enter a valid zip code: ");
+	        userInput = ReadLine();
+	        for(int i = 0; i < zips.Length; i++){
+	            if(userInput == zips[i]){
+	                check = true;
+	                break;
+	            }
+	            else{
+	                check = false;
+	            }
+	        }
+	        if(check == true){
+	            WriteLine("Delivery to "+userInput+" ok");
+	        } else{
+	            WriteLine("Sorry - no delivery to "+userInput);
+	        }
 	}
 }
