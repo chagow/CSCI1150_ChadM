0ba64ddb2857ef9edf3369818cc992f2b4f1884e
chagow <tshadmoua@gmail.com>
2025-03-18 17:30:06 -0500

    Update SalesLetter.cs

--- ex01/SalesLetter.cs
+++ ex01/SalesLetter.cs
@@ -3,8 +3,25 @@ using static System.Console;
 using System.Globalization;
 class SalesLetter
 {
+	static void DisplayContactInfo()
+	{
+		WriteLine("Sea Company");
+		WriteLine("Phone: 555-4334	Cell: 555-04044");
+		WriteLine("Email: seacompany@seasalt.com");
+		WriteLine("On the Web at www.seacompany.com");
+		WriteLine("____________________________");
+		WriteLine("");
+	}
+
 	static void Main()
 	{
-		// Write your code here
+		WriteLine(" From");
+		DisplayContactInfo();
+		WriteLine("Dear Client, ");
+		WriteLine("We want to provide you good service.");
+		WriteLine("Feel free to contact us at any time.");
+		DisplayContactInfo();
+		WriteLine("Looking forward to a long relationship. ");
+		DisplayContactInfo();
 	}
 }
