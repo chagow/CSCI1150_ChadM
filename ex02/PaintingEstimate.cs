1a4f5645a38485cfa0c6fe348af26658b997460f
chagow <tshadmoua@gmail.com>
2025-03-18 18:08:18 -0500

    Update PaintingEstimate.cs

--- ex02/PaintingEstimate.cs
+++ ex02/PaintingEstimate.cs
@@ -3,8 +3,22 @@ using static System.Console;
 using System.Globalization;
 class PaintingEstimate
 {
+	public static void Calculate(int userL, int userW)
+	{
+		int perSQFoot = 6;
+		int height = 9;
+		int lhSQ = (userL * height * 2) * perSQFoot; 
+		int whSq = (userW * height * 2) * perSQFoot;
+		int totalPrice = lhSQ + whSq;
+		WriteLine("Cost of job for {0} X {1} foot room is {2}", userL, userW, 
+		totalPrice.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+	}
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter length of the room in feet: ");
+		int userL = Convert.ToInt32(ReadLine());
+		Write("Enter width of room in feet: ");
+		int userW = Convert.ToInt32(ReadLine());
+		Calculate(userL, userW);
 	}
 }
