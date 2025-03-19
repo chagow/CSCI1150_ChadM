3963d200b82b6839ace913f0b99e8bd5dfab6e66
chagow <tshadmoua@gmail.com>
2025-03-19 02:32:08 -0500

    Update ConvertMilesToKilometers.cs

--- ex03/ConvertMilesToKilometers.cs
+++ ex03/ConvertMilesToKilometers.cs
@@ -3,8 +3,18 @@ using static System.Console;
 using System.Globalization;
 class ConvertMilesToKilometers
 {
+	public static double ConvertToKilometers(double userMiles)
+	{
+		double toKilo = 1.60934;
+		double kiloCalc = userMiles * toKilo;
+		return kiloCalc;
+	}
+
 	static void Main()
 	{
-		// Write your code here
+		Write("Enter number of miles: ");
+		double userMiles = Convert.ToDouble(ReadLine());
+		double kiloCount = ConvertToKilometers(userMiles);
+		WriteLine("{0} miles is {1} kilometers. ", userMiles, kiloCount);
 	}
 }
