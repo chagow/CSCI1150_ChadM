648911588b6155969cdd3cf04b502d7a2b8c4b41
chagow <tshadmoua@gmail.com>
2025-03-19 13:34:53 -0500

    Update FortuneTeller.cs

--- ex04/FortuneTeller.cs
+++ ex04/FortuneTeller.cs
@@ -3,8 +3,25 @@ using static System.Console;
 using System.Globalization;
 class FortuneTeller
 {
+	public static void Fortune(string chosenFortune, string chosenFortune2)
+	{
+		WriteLine(chosenFortune);
+		WriteLine("Also, {0}", chosenFortune2);
+	}
+
 	static void Main()
 	{
-		// Write your code here
+		string[] fortuneTells = {"I see a tall dark stranger in your future.",
+		"You will lose a hundred bucks if you can't pay me.",
+		"You will haunted by the ghost of Claus Santa.",
+		"I can see that you will find a close friend in the near future.",
+		"I sense that the future of the world lies on your shoulder. ",
+		"You may win a lottery around 500 to 1000 dollars. "};
+
+		Random ranNumberGenerator = new Random();
+		int randomNumber = ranNumberGenerator.Next(0, 5);
+		string chosenFortune = fortuneTells[randomNumber];
+		string chosenFortune2 = fortuneTells[randomNumber];
+		Fortune(chosenFortune, chosenFortune2);
 	}
 }
