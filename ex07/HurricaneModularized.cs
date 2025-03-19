4dc0798e806e1b54a14ec677fffd709fbec4b75e
chagow <tshadmoua@gmail.com>
2025-03-19 18:41:12 -0500

    Update HurricaneModularized.cs

--- ex07/HurricaneModularized.cs
+++ ex07/HurricaneModularized.cs
@@ -5,6 +5,34 @@ class HurricaneModularized
 {
 	static void Main()
 	{
-		// Write your code here
+		int user_wind;
+		Write("What is the windspeed of the Hurricane?: ");
+		user_wind = Convert.ToInt32(ReadLine());
+		string hurricaneResult = HurricaneScale(user_wind);
+		WriteLine(hurricaneResult);
+	}
+	public static string HurricaneScale(int user_wind)
+	{
+		string hurricaneCategory = "";
+		if (user_wind < 74){
+			hurricaneCategory = "This is not a hurricane ";
+		} else{
+			if(user_wind < 96 && user_wind >= 74){
+				hurricaneCategory = "This is a category 1 hurricane ";
+			}
+			if(user_wind < 111 && user_wind >= 96){
+				hurricaneCategory = "This is a category 2 hurricane ";
+			}
+			if(user_wind < 130 && user_wind >= 111){
+				hurricaneCategory = "This is a category 3 hurricane ";
+			}
+			if(user_wind < 157 && user_wind >= 130){
+				hurricaneCategory = "This is a category 4 hurricane ";
+			}
+			if(user_wind >= 157){
+				hurricaneCategory = "This is a category 5 hurricane ";
+			}
+		}
+		return hurricaneCategory;
 	}
 }
