3c89a8b9c95c48ee27f5e60566822984378d2304
chagow <tshadmoua@gmail.com>
2025-03-19 18:13:31 -0500

    Update AdmissionModularized.cs

--- ex06/AdmissionModularized.cs
+++ ex06/AdmissionModularized.cs
@@ -5,6 +5,24 @@ class AdmissionModularized
 {
 	static void Main()
 	{
-		// Write your code here
+		int user_score;
+		double user_gpa;
+		Write("Enter high school GPA: ");
+		user_gpa = Convert.ToDouble(ReadLine());
+		Write("Enter admission test score: ");
+		user_score = Convert.ToInt32(ReadLine());
+		string userResult = AcceptOrReject(user_gpa, user_score);
+		WriteLine(userResult);
+	}
+	public static string AcceptOrReject(double user_gpa, int user_score)
+	{
+		string result = "";
+		if (user_gpa >= 3.0 && user_score >= 60 || user_gpa < 3.0 && user_score >= 80){
+			result = "Accept";
+			return result;
+		} else{
+			result = "Reject";
+			return result;
+		}
 	}
 }
