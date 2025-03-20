82cca9253323b71c68045241deb7f907c3edae7f
chagow <tshadmoua@gmail.com>
2025-03-19 20:54:21 -0500

    Update DebugSeven3.cs

--- de03/DebugSeven3.cs
+++ de03/DebugSeven3.cs
@@ -14,27 +14,29 @@ class DebugSeven3
       int quantity;
       double price;
       quantity = GetQuantity();
-      price = Calculateprice(int quantity);
+      price = CalculatePrice(quantity);
       WriteLine("Final price for {0} items is {1}.",
-        quantity, price.ToString("c", CultureInfo.GetCultureInfo("en-US")));
+        quantity, price.ToString("C", CultureInfo.GetCultureInfo("en-US")));
    }
-   private static GetQuantity()
+   private static int GetQuantity()
    {
-      int quan
+      int quantity;
       Write("Enter number of items >> ");
       quantity = Convert.ToInt32(ReadLine());
+      return quantity;
    }
-   private CalculatePrice(int quantityOrdered)
+   private static double CalculatePrice(int quantityOrdered)
    {
        double PRICE_PER_ITEM = 6.00;
        double price = 0;
        double discount = 0;
-       int[] quanLimits = {0, 5, 10, 15;
-       double[] limits = {0, 0.10, 0.14, 0.20;
+       int[] quanLimits = {0, 5, 10, 15};
+       double[] limits = {0, 0.10, 0.14, 0.20};
        for(int x = limits.Length - 1; x >= 0; --x)
-          if(quantityOrdered >= limits[x])
+          if(quantityOrdered >= quanLimits[x]){
               discount = limits[x];
               x = 0;
+          }
        price = quantityOrdered * PRICE_PER_ITEM;
        price = price - price * discount;
        return price;
