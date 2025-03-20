0f49b343061a025a02b1f649300aff0efea02175
chagow <tshadmoua@gmail.com>
2025-03-19 19:36:38 -0500

    Update CountVowelsModularized.cs

--- ex08/CountVowelsModularized.cs
+++ ex08/CountVowelsModularized.cs
@@ -5,6 +5,23 @@ class CountVowelsModularized
 {
 	static void Main()
 	{
-		// Write your code here
+		string input, vowelResult;
+		Write("Insert a phrase: ");
+		input = ReadLine();
+		vowelResult = CountVowels(input);
+		WriteLine("Total amount of vowels: "+vowelResult);
+	}
+	public static string CountVowels(string input)
+	{
+		string total = "";
+		int counter = 0;
+		string lowered = input.ToLower();
+		for(int i = 0; i < lowered.Length; i++){
+			if(lowered[i] == 'a' || lowered[i] == 'e' || lowered[i] == 'i' || lowered[i] == 'o' || lowered[i] == 'u'){
+				counter += 1;
+			}
+		}
+		total = Convert.ToString(counter);
+		return total;
 	}
 }
