4c2f574a98189b527925d0b8595081200b89d652
chagow <tshadmoua@gmail.com>
2025-03-19 19:58:12 -0500

    Update FlexibleArrayMethod.cs

--- ex09/FlexibleArrayMethod.cs
+++ ex09/FlexibleArrayMethod.cs
@@ -5,6 +5,20 @@ class FlexibleArrayMethod
 {
 	static void Main()
 	{
-		// Write your code here
+		int[] array1 = {1, 2, 3};
+		int[] array2 = {4, 5, 6};
+		int[] array3 = {7, 8, 9};
+		DisplayAndSum(array1);
+		DisplayAndSum(array2);
+		DisplayAndSum(array3);
+	}
+	public static void DisplayAndSum(int[] array)
+	{
+		int sum = 0;
+		for(int i = 0; i < array.Length; i++){
+			sum += array[i];
+			Write(array[i]+" ");
+		}
+		WriteLine("The sum of the array is: "+sum);
 	}
 }
