736711628535107840a7e433a27343a171d1d24a
chagow <tshadmoua@gmail.com>
2025-03-26 18:22:42 -0500

    Add files via upload

---
+++ de02/DebugEight2.cs
@@ -0,0 +1,27 @@
+// Overloaded method gives bonus points
+// whether grade is a number or letter
+using static System.Console;
+class DebugEight2
+{
+   static void Main()
+   {
+      int numericScore = 82;
+      string letterScore = B;
+      Write("Score was {0}. ", numericScore);
+      GiveBonus(ref numericScore);
+      WriteLine("Now it is {0}.", numericScore);
+      Write("Grade was ", letterScore);
+      GiveBonus(ref letterScore);
+      WriteLine("Now it is ", letterScore);
+   }
+   public static void giveBonus(int ref testScore)
+   {
+      const int BONUS = 5;
+      testScore = BONUS;
+   }
+   public static void GiveBonus(string ref letterScore)
+   {
+      const string BONUS = "+";
+      letterScore = BONUS;
+   }
+}
