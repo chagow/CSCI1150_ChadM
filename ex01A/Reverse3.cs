c76c1519aa0acc2649a98ba1b929553f872a6fcc
chagow <tshadmoua@gmail.com>
2025-03-26 19:26:44 -0500

    Update Reverse3.cs

--- ex01A/Reverse3.cs
+++ ex01A/Reverse3.cs
@@ -3,8 +3,18 @@ using static System.Console;
 using System.Globalization;
 class Reverse3
 {
+	
 	static void Main()
 	{
-		// Write your code here
+		int firstInt = 23, middleInt = 45, lastInt = 67;
+		WriteLine("{0} {1} {2}", firstInt, middleInt, lastInt);
+		Reverse(ref firstInt, ref middleInt, ref lastInt);
+		WriteLine("{0} {1} {2}", firstInt, middleInt, lastInt);
+	}
+	public static void Reverse(ref int intA, ref int intB, ref int intC)
+	{
+		int temp = intA;
+		intA = intC;
+		intC = temp;
 	}
 }
