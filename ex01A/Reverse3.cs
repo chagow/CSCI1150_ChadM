c3ac8d673c86bbefd8fbc5bf96fdc88ba5bb8d02
chagow <tshadmoua@gmail.com>
2025-03-30 16:10:16 -0500

    Update Reverse3.cs

--- ex01A/Reverse3.cs
+++ ex01A/Reverse3.cs
@@ -1,29 +1,20 @@
-// Overloaded method gives bonus points
-// whether grade is a number or letter
 using System;
 using static System.Console;
 using System.Globalization;
-class DebugEight2
+class Reverse3
 {
-   static void Main()
-   {
-      int numericScore = 82;
-      string letterScore = "B";
-      Write("Score was {0}. ", numericScore);
-      GiveBonus(ref numericScore);
-      WriteLine("Now it is {0}.", numericScore);
-      Write("Grade was {0}. ", letterScore);
-      GiveBonus(ref letterScore);
-      WriteLine("Now it is {0}.", letterScore);
-   }
-   public static void GiveBonus(ref int testScore)
-   {
-      const int BONUS = 5;
-      testScore += BONUS;
-   }
-   public static void GiveBonus(ref string letterScore)
-   {
-      const string BONUS = "+";
-      letterScore += BONUS;
-   }
+	
+	static void Main()
+	{
+		int firstInt = 23, middleInt = 45, lastInt = 67;
+		WriteLine("{0} {1} {2}", firstInt, middleInt, lastInt);
+		Reverse(ref firstInt, ref middleInt, ref lastInt);
+		WriteLine("{0} {1} {2}", firstInt, middleInt, lastInt);
+	}
+	public static void Reverse(ref int intA, ref int intB, ref int intC)
+	{
+		int temp = intA;
+		intA = intC;
+		intC = temp;
+	}
 }
