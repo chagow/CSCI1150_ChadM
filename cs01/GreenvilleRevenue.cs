685d04eb05499d3fc66666a32328bbb4b9f62e69
chagow <tshadmoua@gmail.com>
2025-03-30 18:17:03 -0500

    Update GreenvilleRevenue.cs

--- cs01/GreenvilleRevenue.cs
+++ cs01/GreenvilleRevenue.cs
@@ -1,10 +1,172 @@
 using System;
 using static System.Console;
 using System.Globalization;
+using System.Data;
 class GreenvilleRevenue
 {
    static void Main()
    {
-      // Write your code here
+    double oldcon = 0, newcon = 0, oldcheck, newcheck;
+    double loop = 0;
+    Write("Enter number of contestants last year: ");
+    oldcheck = GetContestantNumber();
+    while(loop == 0){
+        if(oldcheck < 0 || oldcheck > 30){
+            Write("Invalid amount. Re-enter the amount of contestants last year: ");
+            oldcheck = Convert.ToDouble(ReadLine());
+        } else{
+            oldcon = oldcheck;
+            loop += 1;
+        }
+    }
+    Write("Enter number of contestants this year: ");
+    newcheck = GetContestantNumber();
+    while(loop == 1){
+        if(newcheck < 0 || newcheck > 30){
+            Write("Invalid amount. Re-enter the amount of contestants this year: ");
+            newcheck = Convert.ToDouble(ReadLine());
+        } else{
+            newcon = newcheck;
+            loop += 1;
+        }
+    }
+    DisplayRelationship(oldcon, newcon);
+
+    int thisCon = Convert.ToInt32(newcon);
+    string[,] talentNames = new string[thisCon,2];
+    GetContestantData(talentNames);
+    for(int i = 0; i < thisCon; ++i){
+        Write(talentNames[i,0]+" ");
+        for(int j = 0; j < 1; ++j){
+            WriteLine(talentNames[i,1]);
+        }
+    }
+    GetLists(talentNames);
+
+   }
+   public static double GetContestantNumber()
+   {
+      string check;
+      double dataCheck = 0;
+      bool result, loop = true;
+      while(loop == true){
+         check = ReadLine();
+         result = double.TryParse(check, out dataCheck);
+         if(result == true){
+            dataCheck = Convert.ToDouble(check);
+            loop = false;
+         } else{
+            Write("Invalid Input, Try again: ");
+         }
+      }
+      return dataCheck;
+   }
+   public static void DisplayRelationship(double oldcon, double newcon)
+   {
+      const double fee = 25;
+      double revenue = newcon * fee;
+      bool fact = newcon > oldcon;
+      WriteLine("Last year's competition had {0} contestants, and this year's has {1} contestants", oldcon, newcon);
+      WriteLine("Revenue expected this year is {0}", revenue.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+      WriteLine("It is " +fact+ " that this year's competition is bigger than last year's");
+      if (newcon >= oldcon * 2){
+         WriteLine("The competition is more than twice as big this year!");
+      }
+      if (newcon > oldcon && newcon < oldcon * 2){
+         WriteLine("The competition is bigger than ever!");
+      }
+      if(newcon < oldcon){
+         WriteLine("A tighter race this year! Come out and cast your vote!");
+      }
+   }
+   public static string[,] GetContestantData(string[,] array)
+   {
+    int i = 0, j = 0;
+    int rowLength = array.GetLength(0);
+
+    for(i = 0; i < rowLength; ++i){
+        Write("Name {0}: ", i+1);
+        array[i,0] = ReadLine();
+        for(j = 0; j < 1; ++j){
+            Write("Talent (S/D/M/O): ");
+            string talentFix = ReadLine();
+            array[i,1] = talentFix.ToUpper();
+        }
+    }
+    return array;
+   }
+   public static void GetLists(string[,] array)
+   {
+    int loop = 0;
+    int rowLength = array.GetLength(0);
+    int SCount = 0, DCount = 0, MCount = 0, OCount = 0;
+
+    while(loop == 0){
+        for(int i = 0; i < rowLength; ++i){
+            if(array[i,1] == "S"){
+                SCount += 1;
+            }
+            if(array[i,1] == "D"){
+                DCount += 1;
+            }
+            if(array[i,1] == "M"){
+                MCount += 1;
+            }
+            if(array[i,1] == "O"){
+                OCount += 1;
+            }
+        }
+        loop = 1;
+    }
+    WriteLine("The amount of talent are: \nSinging: {0} \nDancing: {1} \nMusical Instrument: {2} \nOther: {3}",
+    SCount, DCount, MCount, OCount);
+    while(loop == 1){
+        Write("What would you like to view (S/D/M/O) or 'Z' to quit: ");
+        string consInput = ReadLine();
+        switch(consInput.ToUpper()){
+            case "S":
+                for(int i = 0; i < rowLength; ++i){
+                    for(int j = 0; j < 1; ++j){
+                        if(array[i,1] == "S"){
+                            WriteLine("{0} ", array[i,0]);
+                        }
+                    }
+                }
+                break;
+            case "D":
+                for(int i = 0; i < rowLength; ++i){
+                    for(int j = 0; j < 1; ++j){
+                        if(array[i,1] == "D"){
+                            WriteLine("{0} ", array[i,0]);
+                        }
+                    }
+                }
+                break;
+            case "M":
+                for(int i = 0; i < rowLength; ++i){
+                    for(int j = 0; j < 1; ++j){
+                        if(array[i,1] == "M"){
+                             WriteLine("{0} ", array[i,0]);
+                        }
+                    }
+                }
+                break;
+            case "O":
+                for(int i = 0; i < rowLength; ++i){
+                    for(int j = 0; j < 1; ++j){
+                        if(array[i,1] == "O"){
+                            WriteLine("{0} ", array[i,0]);
+                        }
+                    }
+                }
+                break;
+            case "Z":
+                loop = 2;
+                break;
+            default:
+                WriteLine("{0} is an invalid code");
+                break;
+        }
+    }
    }
 }
