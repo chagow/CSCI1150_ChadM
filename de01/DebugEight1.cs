0115e1f56c14f218f06a410e0c04dcd6a580d70f
chagow <tshadmoua@gmail.com>
2025-04-01 06:43:08 -0500

    Update DebugEight1.cs

--- de01/DebugEight1.cs
+++ de01/DebugEight1.cs
@@ -11,18 +11,20 @@ class DebugEight1
       int orderNum, quantity;
       double total;
       const double PRICE_EACH = 3.99;
-      GetData(orderNum; quantity);
-      total = quantity * PRICEEACH;
+      GetData(out orderNum, out quantity);
+      total = quantity * PRICE_EACH;
       WriteLine("Order #{0}. Quantity ordered = {1}",
-          orderNum, quantity;
+          orderNum, quantity);
       WriteLine("Total is {0}", total.ToString("C", CultureInfo.GetCultureInfo("en-US")));
    }
-   public static void GetData(out order, out amount)
+   public static void GetData(out int order, out int amount)
    {
        String s1, s2;
        Write("Enter order number ");
-       s1 = ReadLine;
+       s1 = ReadLine();
        Write("Enter quantity ");
-       s2 = ReadLine;
-       order = ConvertToInt32(s1);
-       amount = ConvertToInt32(s2);
+       s2 = ReadLine();
+       order = Convert.ToInt32(s1);
+       amount = Convert.ToInt32(s2);
+   }
+}
