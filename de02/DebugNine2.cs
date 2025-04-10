4e552b360a77e156e6af5358b0f3ed16a45ef9b2
chagow <tshadmoua@gmail.com>
2025-04-10 16:18:12 -0500

    Update DebugNine2.cs

--- de02/DebugNine2.cs
+++ de02/DebugNine2.cs
@@ -8,12 +8,12 @@ class DebugNine2
 {
    static void Main()
    {
-      Breakfast special = new Breakfast("French toast, 4.99);
+      Breakfast special = new Breakfast("French toast", 4.99);
       //Display the info about breakfast
-      WriteLine(INFO);
+      WriteLine(special.INFO);
       // then display today's special
-      WriteLine("Today we are having {1} for {1}",
-         special.name, special.Price.ToString("C2", CultureInfo.GetCultureInfo("en-US")));
+      WriteLine("Today we are having {0} for {1}",
+         special.Name, special.Price.ToString("C2", CultureInfo.GetCultureInfo("en-US")));
   }
 }
 public class Breakfast
@@ -25,9 +25,9 @@ public class Breakfast
    // name, e.g "French toast", and a price
    public Breakfast(string name, double price)
    {
-       name = name;
-       price = price;
+       Name = name;
+       Price = price;
    }
-   public string Name {get; set}
-   public double Price {get; set}
+   public string Name {get; set;}
+   public double Price {get; set;}
 }
