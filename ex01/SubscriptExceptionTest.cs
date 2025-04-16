21eddd0e672993eea053f3062993279c69a598eb
chagow <tshadmoua@gmail.com>
2025-04-16 17:48:25 -0500

    Update SubscriptExceptionTest.cs

--- ex01/SubscriptExceptionTest.cs
+++ ex01/SubscriptExceptionTest.cs
@@ -5,6 +5,30 @@ class SubscriptExceptionTest
 {
 	static void Main()
 	{
-		// Write your code here
+		int loop = 0, userInput = 0;
+		double[] testArray = {20.3, 2, 3, 4, 5, 67.5, 7, 14.6, 9, 10};
+		try
+		{
+			while(loop != 99)
+			{
+				Write("Which of the 10 values would you like to see? (Enter 99 to quit): ");
+				userInput = Convert.ToInt32(ReadLine());
+				if(userInput != 99)
+				{
+					Write("Index {0}: {1,-5}", userInput, testArray[userInput]);
+				}
+				else
+				{
+					if(userInput == 99){
+						loop = userInput;
+					}
+				}
+			}
+		}
+		catch(Exception anExceptionInstance)
+		{
+			WriteLine(anExceptionInstance.ToString());
+			WriteLine("Index {0} was outside the bounds of the array.", userInput);
+		}
 	}
 }
