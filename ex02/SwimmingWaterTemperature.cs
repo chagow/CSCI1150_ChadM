2353987fe7ae614ca586f76b15e3625206cfb188
chagow <tshadmoua@gmail.com>
2025-04-16 19:44:30 -0500

    Update SwimmingWaterTemperature.cs

--- ex02/SwimmingWaterTemperature.cs
+++ ex02/SwimmingWaterTemperature.cs
@@ -5,6 +5,38 @@ class SwimmingWaterTemperature
 {
 	static void Main()
 	{
-		// Write your code here
+		const int MINDEGREE = 70;
+		const int MAXDEGREE = 85;
+		const int freezePoint = 32;
+		const int boilPoint = 212;
+		int loop = 0, userInput = 0;
+		Write("Enter a temperature or 999 to quit: ");
+		while(loop != 999)
+		{
+			userInput = Convert.ToInt32(ReadLine());
+			if(userInput == 999)
+			{
+				loop = userInput;
+			}
+			else
+			{
+				if(userInput <= boilPoint && userInput >= freezePoint)
+				{
+					if(userInput >= MINDEGREE && userInput <= MAXDEGREE)
+					{
+						Write("{0} degrees is comfortable for swimming. ", userInput);
+					}
+					else
+					{
+						Write("{0} degrees is not comfortable for swimming. ", userInput);
+					}
+				}
+				if(userInput > boilPoint || userInput < freezePoint)
+				{
+					throw new ArgumentException("Value does not fall within the expected range.");
+				}
+			}
+			Write("Enter another temperature or 999 to quit: ");
+		}
 	}
 }
