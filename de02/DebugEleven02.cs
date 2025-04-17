571fc0f6653aae5fca00ed2a687450d10f74402e
chagow <tshadmoua@gmail.com>
2025-04-16 20:07:49 -0500

    Update DebugEleven02.cs

--- de02/DebugEleven02.cs
+++ de02/DebugEleven02.cs
@@ -2,21 +2,22 @@
 // Then prevents division by 0
 using System;
 using static System.Console;
+using System.Globalization;
 class DebugEleven02
 {
    static void Main()
    {
       int num = 0, denom = 0;
-      double result;
-      bool dataEntryIsValid;
+      double result = 0;
+      bool dataEntryIsValid = false;
       while(!dataEntryIsValid)
       {
          try
          {
-            DataEntry(num, denom);
+            DataEntry(out num, out denom);
             dataEntryIsValid = true;
          }
-         catch(FormatException e)
+         catch(FormatException)
          {
             WriteLine("Invalid entry - please enter numbers.");
          }
@@ -26,9 +27,9 @@ class DebugEleven02
          result = num * 1.0 / denom;
          WriteLine("Division is successful");
       }
-      catch(DivideByZeroException e)
+      catch(DivideByZeroException)
       {
-         WriteLine("Division failed")
+         WriteLine("Division failed");
          result = 0;
       }
       WriteLine("Result is {0}", result);
@@ -36,10 +37,10 @@ class DebugEleven02
    public static void DataEntry(out int num, out int denom)
    {
       Write("Enter a number ");
-      if(!int.TryParse(ReadLine, out num))
-         Console.WriteLine("Numerator was set to 0");
+      if(!int.TryParse(ReadLine(), out num))
+         Console.WriteLine("Numerator was set to {0}", num);
       Write("Enter a number to divide into the first ");
       if(int.TryParse(ReadLine(), out denom))
-         WriteLine("Denominator was set to 0");
+         WriteLine("Denominator was set to {0}", denom);
    }
 }
