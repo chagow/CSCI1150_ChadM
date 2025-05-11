d41b3dfec95b2e6875cfc917525229f570fb216c
chagow <tshadmoua@gmail.com>
2025-05-11 15:13:51 -0500

    Update DirectoryInformation.cs

--- ex01/DirectoryInformation.cs
+++ ex01/DirectoryInformation.cs
@@ -5,6 +5,28 @@ class DirectoryInformation
 {
    static void Main()
    {
-      // Write your code here
+      string userInput = "";
+      bool loop = true;
+      WriteLine("Enter a directory");
+      while(loop){
+         userInput = ReadLine();
+         if(userInput.ToLower() == "end"){
+            loop = false;
+         }
+         else{
+            if(Directory.Exists(userInput)){
+               WriteLine(userInput + " contains the following files");
+               string[] showFiles = Directory.GetFiles(userInput);
+               for(int i = 0; i < showFiles.Length; ++i){
+                  WriteLine("   {0}", showFiles[i]);
+               }
+               WriteLine("Enter another directory or type end to quit");
+            }
+            else{
+               WriteLine("Directory " + userInput + " does not exist");
+               WriteLine("Enter another directory or type end to quit");
+            }            
+         }
+      }
    }
 }
