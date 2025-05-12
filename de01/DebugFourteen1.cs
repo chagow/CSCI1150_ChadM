9e41faf23a8b869b3cb0d51fc898ea22a811b424
chagow <tshadmoua@gmail.com>
2025-05-11 19:57:30 -0500

    Update DebugFourteen1.cs

--- de01/DebugFourteen1.cs
+++ de01/DebugFourteen1.cs
@@ -8,30 +8,30 @@ class DebugFourteen1
       string fileName;
       string directory;
       string path;
-      string files;
+      string[] files;
       int x;
       Write("Enter a directory: ");
       directory = ReadLine();
-      if(Directory.Exists(Directory))
+      if(Directory.Exists(directory))
       {
           files = Directory.GetFiles(directory);
-          if(files.Length = 0)
+          if(files.Length == 0)
               WriteLine("There are no files in " + directory);
           else
           {
              WriteLine(directory + " contains the following files");
-             for(x = 0; x <= files.Length; ++x)
-                 WriteLine("  " + files[y]);
+             for(x = 0; x < files.Length; ++x)
+                 WriteLine("  " + files[x]);
              Write("\nEnter a file name: ");
              fileName = ReadLine();
-             path = directory + "\\" + fileName;
+             path = directory + "/" + fileName;
              if(File.Exists(path))
              {
                 WriteLine("  File exists and was created " + File.GetCreationTime(path));
                 WriteLine("File was created " + File.GetCreationTime(fileName));
              }
              else
-                 WriteLine("  " + fileName + " does not exist in the " + directory, " directory");
+                 WriteLine("  " + fileName + " does not exist in the " + directory + " directory");
           }
       }
       else
